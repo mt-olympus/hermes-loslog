9c83594d2de85293733c8a531f5f982b7597973e
Leandro Silva <leandro.silva@zoox.com.br>
2016-01-26 10:32:13 -0200

    Incrementing the log output and adding a README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Hermes LosLog
+
+This module provides integration between [Hermes](https://github.com/mt-olympus/hermes) and 
+[LosLog](https://github.com/lansoweb/loslog) to automatically log the requests.
+ 
--- src/Module.php
+++ src/Module.php
@@ -2,10 +2,11 @@
 
 namespace Hermes\LosLog;
 
+use Hermes\Api\Client;
+use Zend\EventManager\Event;
 use Zend\Http\PhpEnvironment\Request;
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
 use Zend\Mvc\MvcEvent;
-use Hermes\Api\Client;
 
 /**
  * @codeCoverageIgnore
@@ -20,12 +21,23 @@ class Module implements AutoloaderProviderInterface
             if (!$e->getRequest() instanceof Request) {
                 return;
             }
+            $request = $e->getRequest();
+
             $config = $serviceLocator->get('Config');
             $serviceName = $config['hermes']['service_name'] ?? '';
             $data = [
-                'service' => $serviceName,
-                'server' => $_SERVER['SERVER_ADDR'],
-                'source_ip' => $_SERVER['REMOTE_ADDR'],
+                'direction' => 'in',
+                'status' => 'success',
+                'destination' => [
+                    'service' => $serviceName,
+                    'server' => $request->getUri()->getHost(),
+                    'uri' => $request->getUriString(),
+                ],
+                'source' => [
+                    'server' => $_SERVER['REMOTE_ADDR'],
+                    'service' => $request->getHeader('X-Request-Name') ? $request->getHeader('X-Request-Name')->getFieldValue() : '',
+                    'uri' => '',
+                ],
             ];
             if (!$e->getRequest()->isGet()) {
                 $post = json_decode($e->getRequest()->getContent(), true, 100);
@@ -36,18 +48,71 @@ class Module implements AutoloaderProviderInterface
         }, 100);
 
         $hermes = $serviceLocator->get('hermes');
-
         $em = $hermes->getEventManager();
-        $em->attach('request.post', function (Client $hermes) {
-            exit;
-            $data = [];
+        $em->attach('request.post', function (Event $e) use ($serviceLocator) {
+            /* @var \Hermes\Api\Client $hermes */
+            $hermes = $e->getTarget();
             $request = $hermes->getZendClient()->getRequest();
+
+            $config = $serviceLocator->get('Config');
+            $serviceName = $config['hermes']['service_name'] ?? '';
+
+            $data = [
+                'direction' => 'out',
+                'status' => 'success',
+                'source' => [
+                    'service' => $serviceName,
+                    'server' => $_SERVER['SERVER_ADDR'],
+                    'uri' => $_SERVER['REQUEST_URI'],
+                ],
+                'destination' => [
+                    'service' => $hermes->getServiceName(),
+                    'server' => $request->getUri()->getHost(),
+                    'uri' => $request->getUriString(),
+                ],
+                'http_code' => $hermes->getZendClient()->getResponse()->getStatusCode(),
+            ];
 
             if (!$request->isGet()) {
                 $post = json_decode($request->getContent(), true, 100);
                 unset($post['password']);
                 $data['data'] = $post;
             }
+            \LosLog\Log\RequestLogger::save($request, $data);
+        }, 100);
+
+        $em->attach('request.fail', function (Event $e) use ($serviceLocator) {
+            /* @var \Hermes\Api\Client $hermes */
+            $hermes = $e->getTarget();
+            $request = $hermes->getZendClient()->getRequest();
+
+            $config = $serviceLocator->get('Config');
+            $serviceName = $config['hermes']['service_name'] ?? '';
+
+            $data = [
+                'direction' => 'out',
+                'status' => 'failed',
+                'source' => [
+                    'service' => $serviceName,
+                    'server' => $_SERVER['SERVER_ADDR'],
+                    'uri' => $_SERVER['REQUEST_URI'],
+                ],
+                'destination' => [
+                    'service' => $hermes->getServiceName(),
+                    'server' => $request->getUri()->getHost(),
+                    'uri' => $request->getUriString(),
+                ],
+            ];
+
+            if (!$request->isGet()) {
+                $post = json_decode($request->getContent(), true, 100);
+                unset($post['password']);
+                $data['data'] = $post;
+            }
+
+            $exception = $e->getParams();
+            $data['http_code'] = $exception->getCode();
+            $data['error'] = $exception->getMessage();
             \LosLog\Log\RequestLogger::save($request, $data);
         }, 100);
     }
